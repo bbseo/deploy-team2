37917409ab3f4fa66a9335427a5d2371f99039e6
bbseo <bbs01011@naver.com>
2022-08-25 17:37:19 +0900

    deploy test

--- src/components/HomeContent.js
+++ src/components/HomeContent.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import map from "../assets/map.png"
-import pin from "../assets/pin.png"
+import map from "../assets/map.png";
+import pin from "../assets/pin.png";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
